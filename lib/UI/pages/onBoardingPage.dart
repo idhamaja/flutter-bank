bd0e68ad4faa36f27a0a4d8b46f138c1f2f6e18d
idham16ip <idham16iramap@gmai.com>
2023-09-29 20:38:01 +0700

    update Flutter Import Assets and SplashScreen

---
+++ lib/UI/pages/onBoardingPage.dart
@@ -0,0 +1,42 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bank/shared/themes.dart';
+
+class OnBoardingPage extends StatelessWidget {
+  const OnBoardingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: lightBackgroundColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CarouselSlider(
+              items: [
+                Image.asset(
+                  'assets/img_onboarding1.png',
+                  height: 331,
+                ),
+                Image.asset(
+                  'assets/img_onboarding2.png',
+                  height: 331,
+                ),
+                Image.asset(
+                  'assets/img_onboarding3.png',
+                  height: 331,
+                ),
+              ],
+              options: CarouselOptions(
+                height: 331,
+                viewportFraction: 1,
+                enableInfiniteScroll: false,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/UI/pages/splashPage.dart
@@ -0,0 +1,47 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bank/UI/pages/onBoardingPage.dart';
+import 'package:flutter_bank/shared/themes.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    Timer(const Duration(seconds: 2), () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const OnBoardingPage(),
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: darkBackgroundColor,
+      body: Center(
+        child: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(
+                'assets/bank_logo2.png',
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
